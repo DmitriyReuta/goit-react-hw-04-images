66b27a83a3866e1d82c86fe82d1a0398dbb4a2e2
Dmitriy Reuta <reutadmitriy@gmail.com>
2023-10-29 11:47:43 +0200

    asda

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
 import { ModalComponent } from "./Modal/Modal";
 import { Searchbar } from "./Searchbar/Searchbar";
-import { MagnifyingGlass } from 'react-loader-spinner'
-export class App extends Component {
-  state = {
-    query: "",
-    images: [],
-    page: 1,
-    isLoading: false,
-    showModal: false,
-    selectedImage: "",
-  };
+import { MagnifyingGlass } from 'react-loader-spinner';
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.fetchImages();
+export function App() {
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+  const [hasMoreImages, setHasMoreImages] = useState(true);
+
+  useEffect(() => {
+    if (query !== "") {
+      setPage(1);
+      setImages([]);
+      setHasMoreImages(true);
+      fetchImages();
     }
-  }
+  }, [query]);
 
-  fetchImages = () => {
-    const { query, page } = this.state;
+  const fetchImages = () => {
     const apiKey = "39227373-dd01e2c6342e880b425481406";
     const perPage = 12;
     const apiUrl = `https://pixabay.com/api/?q=${query}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
 
-    this.setState({ isLoading: true });
+    setIsLoading(true);
 
     axios
       .get(apiUrl)
       .then((response) => {
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...response.data.hits],
-          page: prevState.page + 1,
-        }));
+        setImages((prevImages) => [...prevImages, ...response.data.hits]);
+        setPage((prevPage) => prevPage + 1);
+        setHasMoreImages(response.data.hits.length === perPage);
       })
       .catch((error) => console.error("Error fetching images: ", error))
       .finally(() => {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: "smooth",
@@ -48,44 +48,43 @@ export class App extends Component {
       });
   };
 
-  handleQuerySubmit = (query) => {
-    this.setState({ query, page: 1, images: [] });
+  const handleQuerySubmit = (query) => {
+    setQuery(query);
   };
 
-  handleImageClick = (largeImageURL) => {
-    this.setState({ showModal: true, selectedImage: largeImageURL });
+  const handleImageClick = (largeImageURL) => {
+    setShowModal(true);
+    setSelectedImage(largeImageURL);
   };
 
-  handleCloseModal = () => {
-    this.setState({ showModal: false, selectedImage: "" });
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedImage("");
   };
 
-  render() {
-    const { images, isLoading, selectedImage, page } = this.state;
-    const hasMoreImages = images.length < page * 12;
-
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleQuerySubmit} />
-        <ImageGallery images={images} onImageClick={this.handleImageClick} />
-        {isLoading && <MagnifyingGlass
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="MagnifyingGlass-loading"
-  wrapperStyle={{}}
-  wrapperClass="MagnifyingGlass-wrapper"
-  glassColor = '#c0efff'
-  color = '#e15b64'
-/>}
-        {images.length > 0 && !isLoading && (
-          <Button onClick={this.fetchImages} hasMoreImages={hasMoreImages} />
-        )}
-        <ModalComponent
-          largeImageURL={selectedImage}
-          onRequestClose={this.handleCloseModal}
+  return (
+    <div>
+      <Searchbar onSubmit={handleQuerySubmit} />
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {isLoading && (
+        <MagnifyingGlass
+          visible={true}
+          height={80}
+          width={80}
+          ariaLabel="MagnifyingGlass-loading"
+          wrapperStyle={{}}
+          wrapperClass="MagnifyingGlass-wrapper"
+          glassColor="#c0efff"
+          color="#e15b64"
         />
-      </div>
-    );
-  }
+      )}
+      {!images.length ? null : hasMoreImages && !isLoading && (
+        <Button onClick={fetchImages} hasMoreImages={hasMoreImages} />
+      )}
+      <ModalComponent
+        largeImageURL={selectedImage}
+        onRequestClose={handleCloseModal}
+      />
+    </div>
+  );
 }
